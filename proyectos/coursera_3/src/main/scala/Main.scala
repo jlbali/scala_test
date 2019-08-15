23d9d0f27e2a6713f13ac504f735769ef2fc2fba
lbali <juan.lucas.bali@gmail.com>
2019-08-14 21:01:00 -0300

    Agregando algunas cosas

--- proyectos/coursera_3/src/main/scala/Main.scala
+++ proyectos/coursera_3/src/main/scala/Main.scala
@@ -1,8 +1,12 @@
 
 import example.Hello
+import scalaprog3.weekl.Lesson_3_1
+
 
 object Main extends App {
 
-    val hello = Hello
+    //val hello = Hello
+    val obj = Lesson_3_1
+
 
 }
---
+++ proyectos/coursera_3/src/main/scala/scalaprog3/week1/Lesson.scala
@@ -0,0 +1,8 @@
+package scalaprog3.weekl
+
+object Lesson_3_1 {
+    
+    println("Curso 3, Week 1")
+
+}
+

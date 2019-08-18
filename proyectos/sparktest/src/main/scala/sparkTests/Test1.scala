25834e7d9dd4f3ab5c6c5dd8ba878662d4063ba7
lbali <juan.lucas.bali@gmail.com>
2019-08-18 19:44:44 -0300

    Excepciones con el Spark

--- proyectos/sparktest/src/main/scala/sparkTests/Test1.scala
+++ proyectos/sparktest/src/main/scala/sparkTests/Test1.scala
@@ -3,10 +3,22 @@ package sparkTests
 
 object Test1 {
     println("Corriendo Test1")
+    
     def test1 = {
         import org.apache.spark.{SparkConf, SparkContext}
         import org.apache.spark.SparkContext._
         println("Spark importado")
     }
-    test1
+    
+    def test2 = {
+        import org.apache.spark.{SparkConf, SparkContext}
+        import org.apache.spark.SparkContext._
+        println("Creando context")
+        val conf = new SparkConf().setMaster("local").setAppName("Prueba")
+        val sc = new SparkContext(conf)
+    }
+    
+    
+    //test1
+    test2
 }
